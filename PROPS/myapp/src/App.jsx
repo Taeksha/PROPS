06886c5672d1c2348ec1e71e58fc708e3959c877
Pratiksha Nayak <156405979+Taeksha@users.noreply.github.com>
2024-10-16 20:53:10 +0530

    Add files via upload

---
+++ PROPS/myapp/src/App.jsx
@@ -0,0 +1,36 @@
+
+import './App.css';
+import Image  from './Image';
+
+function App() {
+  return (
+
+
+    <div className="App" style={{display : 'flex', marginTop : '10px'}}>
+
+   <Image src = "https://rukminim2.flixcart.com/image/850/1000/kxp0mfk0/poster/t/t/8/medium-avtar-3d-hollywood-movie-avtaar-3d-matte-finish-poster-original-imaga36q5sxgadsv.jpeg?q=90&crop=false"  height= {300} width = {270} title={"avatar"}>
+
+   </Image>
+   <Image src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWFf-koFGOdDPsfrh-4ld1gXnMpOT64O8Mrg&s" height= {300} width = {270} title={"SPIDER MAN"}></Image>
+   
+   <Image src = "https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg" height= {300} width = {270} title={"IRON MAN"}></Image>
+   
+   <Image src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKKiJveV5ygDMyUiErj7CSZBJPmYHyx6_lAw&s" height= {300} width = {270} title={"HULK"}></Image>
+  
+   <Image src =  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGZuJADFcJaRI0YRizD5bCr5KMueQ2YhKotA&s" height= {300} width = {270} title={"THOR"}></Image>
+   
+    </div>
+
+  );
+  
+} 
+
+ 
+export default App; 
+
+
+
+
+
+
+
